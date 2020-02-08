f5d5406bd4c539214fb81dbe15a6223f6aa74841
RudrenduMahindar <mahindar@usc.edu>
2020-02-08 15:52:48 -0800

    added tests

--- README.md
+++
@@ -1,2 +0,0 @@
-# EE599-HW3-Q2
-Given a vector of integers v, and a number sum, return a vector of two items which are the indices of the two numbers in v such that they add up to sum.  - If there is no answer, the return vector should be empty. - If there are multiple answers, return any of them. - You may not use the same element twice
--- tests/solution_test.cc
+++ tests/solution_test.cc
@@ -2,9 +2,20 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-TEST(HelloWorldShould, ReturnHelloWorld) {
+TEST(VectorIndicesTest, Indicesinvalid) {
   Solution solution;
-  std::string actual = solution.PrintHelloWorld();
-  std::string expected = "**** Hello World ****";
+  vector<int> v={3,7,11,15};
+  int sum=180;
+  vector<int> actual = solution.VectorIndices(v,sum);
+  vector<int> expected = {};
+  EXPECT_EQ(expected, actual);
+}
+
+TEST(VectorIndicesTest, Indicesvalid) {
+  Solution solution;
+  vector<int> v={1,4,3,2};
+  int sum=5;
+  vector<int> actual = solution.VectorIndices(v,sum);
+  vector<int> expected = {0,1};
   EXPECT_EQ(expected, actual);
 }
